81889a99c7f0d196326f1e9ac1befcd55adf1e0f
abzedmohammed <ibraabzed@gmail.com>
2023-02-13 09:55:34 +0300

    deployment to render fix bug

--- my-pesa-backend/build.sh
+++ my-pesa-backend/build.sh
@@ -2,6 +2,15 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+pip install django
+pip install boto3
+pip install django-storages
+pip install djangorestframework
+pip install Pillow
+pip install python-decouple
+pip install python-dotenv
+pip install gunicorn
+pip install django-cors-headers
+pip install psycopg2
 
 python manage.py collectstatic --no-input
